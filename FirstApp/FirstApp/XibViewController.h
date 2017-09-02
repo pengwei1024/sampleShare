dc2a97f08f97573c2364d18fc253af27f251a279
pengwei <pengwei1024@gmail.com>
2017-09-03 00:53:43 +0800

    使用 Xib 布局

---
+++ FirstApp/FirstApp/XibViewController.h
@@ -0,0 +1,13 @@
+//
+//  XibViewController.h
+//  FirstApp
+//
+//  Created by pengwei on 2017/9/2.
+//  Copyright © 2017年 hiandroid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XibViewController : NSObject
+
+@end
---
+++ FirstApp/FirstApp/XibViewController.m
@@ -0,0 +1,13 @@
+//
+//  XibViewController.m
+//  FirstApp
+//
+//  Created by pengwei on 2017/9/2.
+//  Copyright © 2017年 hiandroid. All rights reserved.
+//
+
+#import "XibViewController.h"
+
+@implementation XibViewController
+
+@end
