e635c80f73d49550976a4ca7a3c27d0f31d52943
pengwei <pengwei1024@gmail.com>
2017-12-19 23:28:59 +0800

    CircleProgressView

---
+++ HenCoderSample/circleprogressview/src/main/java/com/apkfuns/circleprogressview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.apkfuns.circleprogressview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    // https://juejin.im/post/5a37067bf265da43248087f7?utm_source=gold_browser_extension
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
