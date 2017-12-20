40727699808ded2c80454b0c8c7cd1849ee66058
pengwei <pengwei1024@gmail.com>
2017-12-20 10:35:16 +0800

    bookPageView

---
+++ HenCoderSample/bookpageview/src/main/java/com/apkfuns/bookpageview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.apkfuns.bookpageview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    // https://juejin.im/post/5a3215c96fb9a045186ac0fe?utm_source=gold_browser_extension
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
